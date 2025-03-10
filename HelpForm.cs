2d56d56101fa7a4e9af920257bc27e87a4ce3302
SystemX <SystemX@DESKTOP-HVDQVNG>
2025-03-10 05:11:17 +0500

    Более менее помощь написана, нужно добавить способности

--- HelpForm.cs
+++ HelpForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿﻿﻿﻿﻿﻿﻿﻿// HelpForm.cs
+using System;
 using System.Windows.Forms;
 
 namespace Saper
@@ -8,23 +9,47 @@ namespace Saper
         public HelpForm()
         {
             InitializeComponent();
-            LoadHelp();
+            LoadHelpContent();
         }
 
-        private void LoadHelp()
+        private void LoadHelpContent()
         {
-            // Здесь можно добавить инструкции по игре
-            Text = "Инструкция по игре:\n" +
-                           "1. Нажмите на клетку, чтобы открыть её.\n" +
-                           "2. Если вы нажмете на мину, игра окончена.\n" +
-                           "3. Цель игры - открыть все клетки, не попав на мины.";
-        }
+            string rules = "Правила игры:\n" +
+                           "1. Цель игры - очистить минное поле, не подорвав ни одной мины.\n" +
+                           "2. Нажмите на ячейку, чтобы открыть её. Если в ней находится мина, игра окончена.\n" +
+                           "3. Щелкните правой кнопкой мыши, чтобы установить флажок на ячейке, чтобы отметить её как подозреваемую мину.\n" +
+                           "4. Игра выиграна, когда все ячейки без мин открыты.\n" +
+                           "5. Используйте таймер, чтобы отслеживать ваш прогресс.\n" +
+                           "6. Вы можете приостановить игру в любое время.\n\n" +
+                           "Советы:\n" +
+                           "- Начинайте с нажатия на углы или края сетки.\n" +
+                           "- Используйте флажки с умом, чтобы отслеживать подозреваемые мины.\n" +
+                           "- Обращайте внимание на числа, которые открываются, так как они указывают, сколько мин находится рядом с этой ячейкой.";
 
-        private void btnBack_Click(object sender, EventArgs e)
-        {
-            MenuForm menuForm = new MenuForm();
-            menuForm.Show();
-            this.Hide();
+
+            TextBox helpTextBox = new TextBox
+            {
+                Text = rules,
+                Dock = DockStyle.Fill,
+                Multiline = true,
+                ScrollBars = ScrollBars.Vertical,
+                ReadOnly = true
+            };
+
+
+            Button closeButton = new Button
+            {
+                Text = "Закрыть",
+                Dock = DockStyle.Bottom
+            };
+            closeButton.Click += (sender, e) => this.Close();
+
+            this.Controls.Add(helpTextBox);
+
+            this.Controls.Add(closeButton);
+            this.Text = "Помощь";
+            this.StartPosition = FormStartPosition.CenterParent;
+            this.Size = new System.Drawing.Size(400, 300);
         }
     }
 }
